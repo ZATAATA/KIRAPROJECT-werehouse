dacaa2d340c03b27234a4220451adf2eb9526f5d
ZATAATA <m3vx123@yahoo.com>
2026-05-17 09:35:17 +0700

    test ke 2

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ import cors from 'cors';
 const app = express();
 const port = 3001;
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173', process.env.FRONTEND_URL].filter(Boolean)
+}));
 app.use(express.json({ limit: '5mb' }));
 
 // Konfigurasi Supabase Client
